f9f6e0661da9d5d6e362435ca9ac6594e6d10f5c
helioramon <helio.alvareng@gmail.com>
2022-07-04 23:14:41 -0300

    Movie list with array and crating a condition with if and else

---
+++ script.js
@@ -0,0 +1,64 @@
+
+let comedy = [{
+    title: 'Elvira: Mistress of the Dark',
+    director: 'James Signorelli',
+    year: 1988
+},
+{
+    title: 'Female Trouble',
+    director: 'John Waters',
+    year: 1974
+},
+{
+    title: 'Sorry to Brother You',
+    director: 'Boots Riley',
+    year: 2019
+}
+]
+
+console.log(comedy)
+
+let action = [{
+    title: 'Run Lola Run',
+    director: 'Tom Tykwer',
+    year: 1998
+},
+{
+    title: 'Matrix',
+    director: 'Lana Wachowski and Lilly Wachowski',
+    year: 1999
+},
+{
+    title: 'Akira',
+    director: 'Katsuhiro Ōtomo',
+    year: 1988
+}
+]
+
+console.log(action)
+
+let fantasy = [{
+    title: 'Good Manners',
+    director: 'Juliana Rojas',
+    year: 2018
+},
+{
+    title: 'Uncle Boonmee Who Can Recall His Past Lives',
+    director: 'Apichatpong Weerasethakul',
+    year: 2010
+},
+{
+    title: 'Mulholland Dr.',
+    director: 'David Lynch',
+    year: 2001
+}
+]
+
+console.log(fantasy)
+
+let lenghtInMinutes = 100
+if (lenghtInMinutes >= 90) {
+    console.log("My favorite movie is Oasis")
+} else {
+    console.log("Not even 5 stars Spencer")
+}
